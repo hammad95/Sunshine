eea76125b0a640dd80e0ff4e53bf727176e5e987
Hammad Khalid <h-hk@live.com>
2016-01-08 17:22:05 -0800

    Add logging statements to observe activity lifecycle

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.android.sunshine.app;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends ActionBarActivity {
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.v(LOG_TAG, "############## onCreate() called ####################");
+
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
@@ -25,5 +31,42 @@ public class MainActivity extends ActionBarActivity {
             actionBar.setLogo(R.drawable.ic_launcher);
             actionBar.setDisplayUseLogoEnabled(true);
         }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.v(LOG_TAG, "############## onStart() called ####################");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.v(LOG_TAG, "############## onResume() called ####################");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.v(LOG_TAG, "############## onPause() called ####################");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.v(LOG_TAG, "############## onStop() called ####################");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.v(LOG_TAG, "############## onDestroy() called ####################");
     }
 }
